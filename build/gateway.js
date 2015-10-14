89ae0fa51f9e6d1aa0c150a617b7b239a448064f
Andrew Reed <andrew@areed.io>
2015-10-14 14:29:52 +0700

    gateway build task is async

--- build/gateway.js
+++ build/gateway.js
@@ -5,10 +5,11 @@ var path = require('path');
 const API_ID = 'qdkkavugcd';
 const STAGE = 'prod';
 
-module.exports = function() {
+module.exports = function(cb) {
   process.chdir('build/aws-apigateway-swagger-importer');
   exec('./aws-api-import.sh --profile peppermint --update ' + API_ID + ' --deploy ' + STAGE + ' ../../swagger.json', function(err, stdout, stderr) {
     console.log(stdout.toString());
     console.log(stderr.toString());
+    cb();
   });
 };
