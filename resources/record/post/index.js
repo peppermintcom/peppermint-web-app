088de609aec4a6bd788bf933e46317b7bac7b7a2
Andrew Reed <andrew@areed.io>
2015-10-20 15:44:06 +0700

    CreateRecord auth and validation errors and unit tests

--- resources/record/post/index.js
+++ resources/record/post/index.js
@@ -1,5 +1,8 @@
 var url = require('url');
+var tv4 = require('tv4');
 var _ = require('utils');
+
+var bodySchema = _.bodySchema(require('./spec').parameters);
 
 exports.handler = function(request, reply) {
   var jwt = _.authenticate(request.Authorization);
@@ -8,7 +11,13 @@ exports.handler = function(request, reply) {
     return;
   }
 
-  var parts = url.parse(request.body.signed_url);  
+  var isValid = tv4.validate(request.body, bodySchema);
+  if (!isValid) {
+    reply.fail(['Bad Request:', tv4.error.message].join(' '));
+    return;
+  }
+
+  var parts = url.parse(request.body.signed_url);
 
   reply.succeed({
     canonical_url: url.format(_.omit(parts, ['search', 'query']))
--- resources/record/post/test.js
+++ resources/record/post/test.js
@@ -4,22 +4,100 @@ var _ = require('utils');
 
 describe('lambda:CreateRecord', function() {
   var jwt = _.jwt(null, 1);
+  var signedURL = 'https://peppermint-cdn.s3.amazonaws.com/AW3/cpLPai2DIqcETFtWsn0cWc?AWSAccessKeyId=AKIAJZTQ4SASPHAFE5AQ&Expires=1445220215&Signature=47q4xCdhIc89K0SMm2YHH%2BQIAdI%3D';
 
-  describe('given a valid signed_url', function() {
-    it('should return a publicly readable url', function(done) {
-      handler({
-        Authorization: 'Bearer ' + jwt,
-        body: {
-          signed_url: 'https://peppermint-cdn.s3.amazonaws.com/AW3/cpLPai2DIqcETFtWsn0cWc?AWSAccessKeyId=AKIAJZTQ4SASPHAFE5AQ&Expires=1445220215&Signature=47q4xCdhIc89K0SMm2YHH%2BQIAdI%3D',
-        },
-      }, {
-        fail: function(err) {
-          done(new Error(err));
-        },
-        succeed: function(res) {
-          expect(res).to.have.property('canonical_url', 'https://peppermint-cdn.s3.amazonaws.com/AW3/cpLPai2DIqcETFtWsn0cWc');
-          done();
-        }
+  describe('Valid Requests', function() {
+    describe('given a valid signed_url', function() {
+      it('should return a publicly readable url', function(done) {
+        handler({
+          Authorization: 'Bearer ' + jwt,
+          body: {
+            signed_url: signedURL,
+          },
+        }, {
+          fail: function(err) {
+            done(new Error(err));
+          },
+          succeed: function(res) {
+            expect(res).to.have.property('canonical_url', 'https://peppermint-cdn.s3.amazonaws.com/AW3/cpLPai2DIqcETFtWsn0cWc');
+            done();
+          }
+        });
+      });
+    });
+  });
+
+  describe('Invalid', function() {
+    describe('Missing Authorization field', function() {
+      it('should fail with an Unauthorized error.', function(done) {
+        handler({
+          body: {
+            signed_url: signedURL,
+          },
+        }, {
+          fail: function(err) {
+            expect(err).to.match(/Unauthorized/);
+            done();
+          },
+          succeed: function() {
+            done(new Error('succeed'));
+          },
+        });
+      });
+    });
+
+    describe('Empty Authorization field', function() {
+      it('should fail with an Unauthorized error.', function(done) {
+        handler({
+          Authorization: '',
+          body: {
+            signed_url: signedURL
+          },
+        }, {
+          fail: function(err) {
+            expect(err).to.match(/Unauthorized/);
+            done();
+          },
+          succeed: function() {
+            done(new Error('succeed'));
+          },
+        });
+      });
+    });
+
+    describe('Malformed Authorization field', function() {
+      it('should fail with an Unauthorized error.', function(done) {
+        handler({
+          Authorization: jwt,
+          body: {
+            signedURL: 'audio/mp3',
+          },
+        }, {
+          fail: function(err) {
+            expect(err).to.match(/Unauthorized/);
+            done();
+          },
+          succeed: function() {
+            done(new Error('succeed'));
+          },
+        });
+      });
+    });
+
+    describe('Missing signed_url in body', function() {
+      it('should fail with a 400 error.', function(done) {
+        handler({
+          Authorization: 'Bearer ' + jwt,
+          body: {},
+        }, {
+          fail: function(err) {
+            expect(err).to.match(/Bad Request.*signed_url/)
+            done();
+          },
+          succeed: function() {
+            done(new Error('succeed'));
+          },
+        })
       });
     });
   });
