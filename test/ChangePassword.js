c93a9a5f35ea997cd5aa05fbc1ac3dce3f9e560a
Andrew Reed <andrew@areed.io>
2015-11-18 10:51:53 +0700

    reset password integration test
    Default to skipped because tester must check email for link then use form on
    peppermint.com/reset to manually change password.

--- test/ChangePassword.js
+++ test/ChangePassword.js
@@ -1,10 +1,10 @@
+var readline = require('readline');
 var expect = require('chai').expect;
-var _ = require('utils/test');
 
-//depends on password reset form on peppermint.com
-describe.skip('PUT /accounts', function() {
+//uses password reset form on peppermint.com
+describe.skip('Forgot Password', function() {
   var user = _.fake.user();
-  var pass = 'my new password';
+  var pass = 'secret2';
   var jwt;
 
   before(function() {
@@ -20,28 +20,50 @@ describe.skip('PUT /accounts', function() {
 
   after(_.deleteAccountAfter(user.email));
 
-  describe('with a valid auth token', function() {
-    it('should update the password.', function() {
-      return _.http('PUT', '/accounts', {
+  describe('POST /accounts/recover', function() {
+    it('should send an email with a password reset link.', function() {
+      return _.http('POST', '/accounts/recover', {
           api_key: _.fake.API_KEY,
-          u: {
-            password: pass,
-          },
+          email: user.email,
         })
         .then(function(res) {
-          expect(res.statusCode).to.equal(201);
-          expect(res.body).to.deep.equal({});
-
-          return _.http('POST', '/accounts/tokens', {
-            api_key: _.fake.API_KEY,
-          }, {
-            Authorization: _.basic(user.email, pass),
-          });
-        })
-        .then(function(res) {
-          console.log(res.statusCode);
-          console.log(res.body);
           expect(res.statusCode).to.equal(200);
+        });
+    });
+  });
+
+  //depends on above POST /accounts/recover test
+  //The reset form uses the PUT /accounts method
+  describe('change password at peppermint.com/reset', function() {
+    before(function(done) {
+      this.timeout(120000);
+
+      var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+
+      rl.question([
+        'Use password reset link emailed to',
+        user.email,
+        'to change password to',
+        pass,
+        'then hit enter.',
+      ].join(' '), function() {
+        rl.close();
+        done();
+      });
+    });
+
+    it('should be able to authenticate with new password.', function() {
+      return _.http('POST', '/accounts/tokens', {
+          api_key: _.fake.API_KEY,
+        }, {
+          Authorization: _.basic(user.email, pass),
+        })
+        .then(function(res) {
+          expect(res.statusCode).to.equal(200);
+          expect(res.body).to.have.property('at');
         });
     });
   });
