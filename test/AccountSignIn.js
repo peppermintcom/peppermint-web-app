c539fc0378ae01628f836bb3dc5369016c3b31ff
Andrew Reed <andrew@areed.io>
2015-11-27 15:15:28 +0700

    Revert "GET /accounts 404 precedence over 401"
    This reverts commit 3ecb15adef93effa016cb1ccdbbfd3bfe622004a.

--- test/AccountSignIn.js
+++ test/AccountSignIn.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var http = require('request');
 var _ = require('utils/test');
 var post = _.partial(_.http, 'POST', '/accounts/tokens', {api_key: _.fake.API_KEY});
 
