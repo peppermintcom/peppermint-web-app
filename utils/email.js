e9f54d7d05b1549c1ed3153663c96ce9a39afa45
Andrew Reed <andrew@areed.io>
2015-12-28 09:34:54 +0700

    load smtp credentials from conf file

--- utils/email.js
+++ utils/email.js
@@ -1,8 +1,9 @@
 var emailjs = require('emailjs/email');
+var conf = require('./conf.js');
 
 module.exports = emailjs.server.connect({
-  user: process.env.PEPPERMINT_SES_SMTP_USER,
-  password: process.env.PEPPERMINT_SES_SMTP_PASS,
+  user: conf.PEPPERMINT_SES_SMTP_USER,
+  password: conf.PEPPERMINT_SES_SMTP_PASS,
   host: 'email-smtp.us-west-2.amazonaws.com',
   ssl: true,
 });
