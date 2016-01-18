2bdeaa3ad796c4c78ba4e26aa6be49d5bb12420e
Andrew Reed <andrew@areed.io>
2016-01-18 13:50:28 +0700

    200 response code for success
    Can't set Location header so 201 is inappropriate.

--- resources/jwts/post/spec.js
+++ resources/jwts/post/spec.js
@@ -8,7 +8,7 @@ exports.tags = ['accounts', 'recorder', 'auth'];
 exports.summary = 'Exchange credentials for an access token.';
 exports.description = 'The Peppermint API authenticates both recorders and accounts. While most opeartions only require the client be authenticated as one or the other, some API operations require the client to be authenticated as both recorder and account. This endpoint uses a custom scheme in the Authorization header to accept credentials for both recorders and accounts and respond with a token asserting the bearer is both an account and recorder. It also accepts a single set of credentials to support clients that do not need to authenticate as both account and recorder.';
 exports.operationId = 'Authenticate';
-exports.consumes = exports.produces = ['application/vnd.api+json'];
+exports.produces = ['application/vnd.api+json'];
 
 exports.parameters = [
   {
@@ -72,10 +72,10 @@ exports['x-amazon-apigateway-integration'] = {
   httpMethod: 'POST',
   credentials: 'arn:aws:iam::819923996052:role/APIGatewayLambdaExecRole',
   requestTemplates: {
-    'application/vnd.api+json': integrations.requestTmplNoBody,
+    'application/json': integrations.requestTmplNoBody,
   },
   responses: {
-    'default': integrations.Created,
+    'default': integrations.Ok,
     'Bad Request.*': integrations.BadRequest,
     'Unauthorized.*': integrations.Unauthorized,
     'Not Found.*': integrations.NotFound,
