7e13c13d65932539d13562d647e3ee4e76ed2a5f
Andrew Reed <andrew@areed.io>
2016-02-25 08:58:23 +0700

    search messages pagination integration test

--- test/SearchMessages.js
+++ test/SearchMessages.js
@@ -1,7 +1,8 @@
 var expect = require('chai').expect;
 var _ = require('utils/test');
 
-describe.only('GET /messages', function() {
+describe('GET /messages', function() {
+  this.timeout(5 * 60 * 1000);
   var recipient;
   var recipientJWT;
   
@@ -21,53 +22,41 @@ describe.only('GET /messages', function() {
     });
   });
 
-  describe('recipient received 2 messages in 2015 and 2 in 2016', function() {
+  describe('recipient received 80 messages in 2015 and 2 in 2016', function() {
     var messages;
 
     before(function() {
-      var msgs = [{
-        recipient_email: recipient.email,
-        sender_email: _.fake.user().email,
-        audio_url: _.fake.AUDIO_URL,
-        created: new Date('2015-12-31T12:59:59').valueOf(),
-        message_id: _.token(22),
-      }, {
-        recipient_email: recipient.email,
-        sender_email: _.fake.user().email,
-        audio_url: _.fake.AUDIO_URL,
-        created: new Date('2015-12-31T12:59:59').valueOf(),
-        message_id: _.token(22),
-      }, {
-        recipient_email: recipient.email,
-        sender_email: _.fake.user().email,
-        audio_url: _.fake.AUDIO_URL,
-        created: new Date('2016-01-01T00:00:01').valueOf(),
-        message_id: _.token(22),
-      }, {
+      var created = new Date('2015-12-01T12:00:00').valueOf();
+
+      var msgs = _.map(_.range(80), function(i) {
+        return {
+          recipient_email: recipient.email,
+          sender_email: _.fake.user().email,
+          audio_url: _.fake.AUDIO_URL,
+          created: created + i*1000,
+          message_id: _.token(22),
+        };
+      });
+
+      //2016 messages
+      msgs.push({
         recipient_email: recipient.email,
         sender_email: _.fake.user().email,
         audio_url: _.fake.AUDIO_URL,
         created: new Date('2016-01-01T00:00:01').valueOf(),
         message_id: _.token(22),
-      }];
+      });
+      msgs.push({
+        recipient_email: recipient.email,
+        sender_email: _.fake.user().email,
+        audio_url: _.fake.AUDIO_URL,
+        created: new Date('2016-01-01T00:00:02').valueOf(),
+        message_id: _.token(22),
+      });
 
       return Promise.all(_.map(msgs, function(msg) {
         return _.messages.put(msg);
       }));
-    });
-
-    describe('?recipient=:id', function() {
-      it('should return 4 messages.', function() {
-        return _.http('GET', '/messages?recipient=' + recipient.account_id, null, {
-          'X-Api-Key': _.fake.API_KEY,
-          Authorization: 'Bearer ' + recipientJWT,
-        })
-        .then(function(res) {
-          expect(res.statusCode).to.equal(200);
-          expect(res.body).to.have.property('data');
-          expect(res.body.data).to.have.length(4);
-        });
-      });
     });
 
     describe('?recipient=:id&since=2016-01-01 00:00:00', function() {
@@ -80,7 +69,47 @@ describe.only('GET /messages', function() {
           expect(res.statusCode).to.equal(200);
           expect(res.body).to.have.property('data');
           expect(res.body.data).to.have.length(2);
+          expect(res.body).not.to.have.property('links');
         });
+      });
+    });
+
+    describe('?recipient=:id&since=2017-01-01 00:00:00', function() {
+      it('should return no messages.', function() {
+
+      });
+    });
+
+    describe('?recipient=:id', function() {
+      it('should return 2 pages of 40 messages then 2 messages on the last page.', function() {
+        var headers = {
+          'X-Api-Key': _.fake.API_KEY,
+          Authorization: 'Bearer ' + recipientJWT,
+        };
+
+        return _.http('GET', '/messages?recipient=' + recipient.account_id, null, headers)
+          .then(function(res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.have.property('data');
+            expect(res.body.data).to.have.length(40);
+            expect(res.body).to.have.property('links');
+
+            return _.http('GET', res.body.links.next, null, headers)
+          })
+          .then(function(res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.have.property('data');
+            expect(res.body.data).to.have.length(40);
+            expect(res.body).to.have.property('links');
+
+            return _.http('GET', res.body.links.next, null, headers)
+          })
+          .then(function(res) {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.have.property('data');
+            expect(res.body.data).to.have.length(2);
+            expect(res.body).not.to.have.property('links');
+          });
       });
     });
   });
