1f0cea8e832f990619f69bff561c5f864bec6ad0
Andrew Reed <andrew@areed.io>
2016-05-23 16:04:53 +0700

    facebook and google lambda:SignIn tests

--- lambda/test/SignIn.js
+++ lambda/test/SignIn.js
@@ -2,6 +2,7 @@ import {expect} from 'chai'
 import tv4 from 'tv4'
 import defs from '../../definitions'
 import {SignIn} from '../bundle.js'
+import domain from '../../domain'
 import fixtures from '../../repository/fixtures'
 import accounts from '../../repository/accounts'
 import jwts from '../../utils/jwt'
@@ -13,6 +14,101 @@ const FACEBOOK = 2;
 
 describe('lambda:SignIn', function() {
   describe('no include param', function() {
+
+    describe.only('goole', function() {
+      var email = 'andrew@areed.io';
+      var name = 'Andrew Reed';
+      //https://developers.google.com/oauthplayground/
+      var accessToken = process.env.GOOGLE_AT;
+
+      describe('account does not exist', function() {
+        before(function() {
+          return accounts.delete(email)
+        })
+
+        it('should return a jwt and new account.', function(done) {
+          SignIn({
+            Authorization: peppermintScheme(null, null, email, accessToken, GOOGLE),
+            api_key: fixtures.API_KEY,
+          }, null, function(err, res) {
+
+            if (err) return done(err)
+
+            if (!tv4.validate(res, spec.responses['200'])) {
+              throw tv4.error
+            }
+
+            //main resource is a jwt
+            let jwt = jwts.verify(res.data.attributes.token)
+            expect(jwt).to.have.property('account_id')
+
+            //account is included
+            if (!tv4.validate(res.included[0], defs.accounts.schema)) {
+              throw tv4.error
+            }
+            expect(res.included[0].id).to.equal(res.data.relationships.account.data.id)
+            done()
+          })
+        })
+      })
+
+      describe('account exists', function() {
+        let account
+
+        before(function() {
+          return accounts.upsert(domain.newAccount({
+            email: email,
+            full_name: name,
+            verification_source: 'Mocha',
+          }))
+          .then(function(_account) {
+            account = _account
+          })
+        })
+
+        it('should return a jwt and the existing account.', function(done) {
+          SignIn({
+            Authorization: peppermintScheme(null, null, email, accessToken, GOOGLE),
+            api_key: fixtures.API_KEY,
+          }, null, function(err, res) {
+            if (err) return done(err)
+
+            if (!tv4.validate(res, spec.responses['200'])) {
+              throw tv4.error
+            }
+
+            let includedAccount = res.included[0];
+
+            if (!tv4.validate(includedAccount, defs.accounts.schema)) {
+              throw tv4.error;
+            }
+            expect(includedAccount.id).to.equal(account.account_id);
+            expect(includedAccount.attributes).to.have.property('email', account.email);
+            done()
+          })
+        })
+      })
+
+      describe('invalid access token', function() {
+        it('should fail with a 400 error.', function(done) {
+          SignIn({
+            Authorization: peppermintScheme(null, null, email, 'x' + accessToken, GOOGLE),
+            api_key: fixtures.API_KEY,
+          }, null, function(err, res) {
+            if (!err) done(new Error('success with bad access token'))
+
+            try {
+              expect(err).to.have.property('message', '401')
+              expect(JSON.parse(err.name)).to.deep.equal({
+                detail: 'Google rejected access token',
+              })
+            } catch(e) {done(e)}
+            done()
+          })
+        })
+      })
+    })
+
     describe('facebook', function() {
       var email = 'andrew@areed.io';
       var name = 'Andrew Reed';
@@ -50,68 +146,61 @@ describe('lambda:SignIn', function() {
         });
       });
 
-        /*
       describe('account exists', function() {
-        var result, account;
+        let account
 
         before(function() {
-          return _.accounts.upsert({
+          return accounts.upsert(domain.newAccount({
             email: email,
             full_name: name,
-            source: 'Mocha',
-          })
+            verification_source: 'Mocha',
+          }))
           .then(function(_account) {
-            account = _account;
-          });
-        });
+            account = _account
+          })
+        })
 
-        before(function(done) {
-          handler({
-            Authorization: _.peppermintScheme(null, null, email, accessToken, FACEBOOK),
-            api_key: _.fake.API_KEY,
-          }, {
-            succeed: function(_result) {
-              result = _result;
-              done();
-            },
-            fail: done,
-          });
-        });
+        it('should return a jwt and the existing account.', function(done) {
+          SignIn({
+            Authorization: peppermintScheme(null, null, email, accessToken, FACEBOOK),
+            api_key: fixtures.API_KEY,
+          }, null, function(err, res) {
+            if (err) return done(err)
 
-        it('should succeed with a JWT.', function() {
-          expect(result.data.attributes.token).to.be.ok;
-        });
+            if (!tv4.validate(res, spec.responses['200'])) {
+              throw tv4.error
+            }
 
-        it('should include the account.', function() {
-          var includedAccount = result.included[0];
+            let includedAccount = res.included[0];
 
-          if (!tv4.validate(includedAccount, defs.accounts.schema)) {
-            throw tv4.error;
-          }
-          expect(includedAccount.id).to.equal(account.account_id);
-          expect(includedAccount.attributes).to.have.property('email', account.email);
-        });
+            if (!tv4.validate(includedAccount, defs.accounts.schema)) {
+              throw tv4.error;
+            }
+            expect(includedAccount.id).to.equal(account.account_id);
+            expect(includedAccount.attributes).to.have.property('email', account.email);
+            done()
+          })
+        })
       });
 
       describe('invalid access token', function() {
-        it('should fail with a 400 error.', function() {
-          handler({
-            Authorization: _.peppermintScheme(null, null, email, 'x' + accessToken, FACEBOOK),
-            api_key: _.fake.API_KEY,
-          }, {
-            succeed: function() {
-              done(new Error('success with bad access token'));
-            },
-            fail: function() {
-              expect(err).to.have.property('message', '401');
+        it('should fail with a 400 error.', function(done) {
+          SignIn({
+            Authorization: peppermintScheme(null, null, email, 'x' + accessToken, FACEBOOK),
+            api_key: fixtures.API_KEY,
+          }, null, function(err, res) {
+            if (!err) done(new Error('success with bad access token'))
+
+            try {
+              expect(err).to.have.property('message', '401')
               expect(JSON.parse(err.name)).to.deep.equal({
-                errors: [{detail: 'Facebook rejected access token'}],
-              });
-            },
-          });
-        });
-      });
-      */
-    });
+                detail: 'Facebook rejected access token',
+              })
+            } catch(e) {done(e)}
+            done()
+          })
+        })
+      })
+    })
   })
 })
